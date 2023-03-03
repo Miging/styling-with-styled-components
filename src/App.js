f7c3ec80b0e88e11773907c385064e4f8a179917
Miging <dnfldpden32@gmail.com>
2023-03-04 00:31:17 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled,{css, ThemeProvider} from 'styled-components';
+import Button from './components/Button';
 
+
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin:0 auto;
+  margin-top:4rem;
+  border:1px solid black;
+  padding:1rem;
+`;
+
+const ButtonGroup=styled.div`
+  &+&{
+    margin-top:1rem;
+  }
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue:'#228be6',
+          gray:'#495057',
+          pink:'#f06595'
+        }
+      }}
+    >
+      <AppBlock>
+        <ButtonGroup>
+          <Button size='large'>BUTTON</Button>
+          <Button >BUTTON</Button>
+          <Button size='small'>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size='large' color='gray'>BUTTON</Button>
+          <Button color='gray' >BUTTON</Button>
+          <Button size='small' color='gray'>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size='large' color='pink'>BUTTON</Button>
+          <Button color='pink'>BUTTON</Button>
+          <Button size='small' color='pink'>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size='large' outline>BUTTON</Button>
+          <Button color='pink' outline>BUTTON</Button>
+          <Button size='small' color='gray' outline>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size='large' fullWidth>BUTTON</Button>
+          <Button color='pink' fullWidth>BUTTON</Button>
+          <Button size='small' color='gray' fullWidth>BUTTON</Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
